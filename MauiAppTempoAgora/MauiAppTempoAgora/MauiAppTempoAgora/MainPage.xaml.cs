1f1f22800621edfdf3c6abe0efcfe28d90be40e9
Fernando-Onishi <onishi.fernando2306@gmail.com>
2026-04-05 16:56:16 -0300

    .

---
+++ MauiAppTempoAgora/MauiAppTempoAgora/MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if(t != null) 
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao =
+                                         $"Descrição: {t.description} \n" +
+                                         $"Velocidade do vento: {t.speed} \n" +
+                                         $"Visibilidade: {t.visibility} \n" +
+                                         $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+                    } else
+                    {
+
+                        lbl_res.Text = "Cidade não encontrada";
+                    }
+
+                } else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Sem internet", "Você está sem conexão com a internet.", "OK");
+            }
+        }
+    }
+
+}
